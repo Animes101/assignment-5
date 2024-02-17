ce15524789599af1a2eaf299e1f240a56ec9e860
animes barman <animesbarman101@gmail.com>
2024-02-18 01:14:42 +0600

    start javascript

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//select all seat button and click Event add
+
+const seatsButtons=document.querySelectorAll('#sit-button');
+for(const seatButton of seatsButtons){
+    seatButton.addEventListener('click',function(e){
+        
+        
+    })
+}
